8ac006600d4ca4aa995059ed9165889945e9a910
purushothamanzenylog <reactjs.1@zenylog.com>
2023-07-22 14:04:47 +0530

    changes

--- src/Modules/Group/Screen/index.tsx
+++ src/Modules/Group/Screen/index.tsx
@@ -1,9 +1,10 @@
-import { Button, Modal, Input, CommonTable, NoDataFound, Card, DropDown, NoRecordsFound, Spinner } from '@Components';
+import { Button, Modal, Input, CommonTable, NoDataFound, Card, DropDown, NoRecordsFound, Spinner, MenuBar } from '@Components';
 import { useDynamicHeight, useInput, useLoader, useModal, useNavigation } from '@Hooks'
 import { createKnowledgeGroup, createKnowledgeGroupVariant, getKnowledgeGroup, getKnowledgeGroupVariant } from '@Redux';
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { translate } from "@I18n";
+import { icons } from '@Assets';
 
 
 function Group() {
@@ -17,11 +18,14 @@ function Group() {
     const dispatch = useDispatch()
     const dynamicHeight: any = useDynamicHeight()
     const GroupSubmitLoader = useLoader(false);
-    const [showGroup, setShowGroup] = useState(false);
+    const [variant, setVariant] = useState<any>();
     const VariantSubmitLoader = useLoader(false);
     const [selectedGroupDetails, setSelectedGroupDetails] = useState<any>({})
     const [convertedGroupDetails, setConvertedGroupDetails] = useState<any>([])
     const [selectedGroup, setSelectedGroup] = useState<any>(undefined);
+    const [selectedGroupVariant, setSelectedGroupVariant] = useState<any>();
+    const MENU = [{ id: '0', name: "Edit", icon: icons.edit }]
+
 
     // createKnowledgeGroupVariant
     useEffect(() => {
@@ -35,13 +39,13 @@ function Group() {
             getKnowledgeGroup({
                 params,
                 onSuccess: (success: any) => () => {
-                    // const normalizedArray = success?.details?.knowledege_groups && success?.details?.knowledege_groups.length > 0
-                    //     && success.details.knowledege_groups.map((el: any) => {
-                    //         let requiredObject = { show: false }
-                    //         return { ...el, ...requiredObject }
-                    //     })
+                    const normalizedArray = success?.details?.knowledege_groups && success?.details?.knowledege_groups.length > 0
+                        && success.details.knowledege_groups.map((el: any) => {
+                            let requiredObject = { show: false }
+                            return { ...el, ...requiredObject }
+                        })
 
-                    // setConvertedGroupDetails(normalizedArray)
+                    setConvertedGroupDetails(normalizedArray)
                 },
                 onError: (error: string) => () => {
                 },
@@ -55,7 +59,8 @@ function Group() {
             getKnowledgeGroupVariant({
                 params,
                 onSuccess: (success: any) => () => {
-                    filteredGroupDetails(success.details)
+                    // filteredGroupDetails(success.details)
+                    setVariant(success.details)
                 },
                 onError: (error: string) => () => {
                 },
@@ -77,7 +82,7 @@ function Group() {
                 params,
                 onSuccess: (success: any) => () => {
                     GroupSubmitLoader.hide()
-                    variantModal.hide()
+                    editModal.hide()
                 },
                 onError: (error: string) => () => {
                     GroupSubmitLoader.hide()
@@ -94,8 +99,6 @@ function Group() {
             description: description?.value,
             knowledge_group_id: selectedGroupDetails?.id
         };
-
-
         dispatch(
             createKnowledgeGroupVariant({
                 params,
@@ -103,7 +106,7 @@ function Group() {
                     VariantSubmitLoader.hide()
                     variantModal.hide()
                     getKnowledgeGroupDetails()
-
+                    getKnowledgeGroupVariantDetails()
                 },
                 onError: (error: string) => () => {
                     VariantSubmitLoader.hide()
@@ -117,96 +120,48 @@ function Group() {
         variantModal.show()
     }
 
-    // const onClickShow = (item: any, index: number) => {
-    //     console.log("=========>", convertedGroupDetails);
-    //     let showVariant = [...convertedGroupDetails]
-    //     showVariant && showVariant.length > 0 && showVariant.map((el: any) => {
-    //         if (showVariant[index].show) {
-    //             showVariant[index].show = false
-    //         } else {
-    //             showVariant[index].show = true
-    //         }
-    //     })
-    //     setConvertedGroupDetails(showVariant)
-    // }
+    const onClickShow = (item: any, index: number) => {
+        let showVariant = convertedGroupDetails.map((el: any, i: number) => {
+            return {
+                ...el,
+                show: i === index ? !el.show : false
+            };
+        });
+        setConvertedGroupDetails(showVariant);
+    };
 
 
-    const filteredGroupDetails = (response) => {
-        let GroupDetailsCopy = [...GroupDetails?.knowledege_groups];
-        response && response?.knowledge_group_varaiant.length > 0 && response.knowledge_group_varaiant.map((element: any, index: number) => {
-            GroupDetailsCopy && GroupDetailsCopy.length > 0 && GroupDetailsCopy.map((el: any) => {
-                if (element?.knowledge_group?.id === el.id) {
-                    GroupDetailsCopy[index].variant = element
-                }
-            })
+    const selectedVariant = (el: any) => {
+        let filteredVariant: any = []
+        variant && variant?.knowledge_group_varaiant.length > 0 && variant.knowledge_group_varaiant.map((element: any) => {
+            if (element?.knowledge_group?.id === el.id) {
+                filteredVariant = [...filteredVariant, element]
+            }
         })
-        setConvertedGroupDetails(GroupDetailsCopy)
+        setSelectedGroupVariant(filteredVariant)
+    }
+
+    const editVariant = (item: any) => {
+        console.log("edit variant", item);
+
     }
 
     const normalizedTaskGroupData = (data: any) => {
-        return data.map((taskGroup: any,) => {
-
-            console.log("==========>", taskGroup);
-
-
+        return data && data.length > 0 && data.map((variant: any) => {
             return {
                 name: <div className="row  align-items-center">
                     <div className="pl-3">
-                        <span className={`'text-primary'}`}>{taskGroup.variant?.name}</span>
-                        <br></br>
+                        <span className={`'text-primary'}`}>{variant?.name}</span>
                     </div>
                 </div >,
-                tag: '',
-                // "": <MenuBar menuData={getGroupMenuItem(marked_as_closed, is_parent)} onClick={(el) => {
-                //   if (el.id === '0') {
-                //     if (is_parent) {
-                //       addTaskGroupModal.show()
-                //       setSelectedTaskGroup(taskGroup)
-                //       const { name, description, code, photo } = taskGroup
-                //       taskGroupName.set(name)
-                //       taskGroupDescription.set(description)
-                //       taskGroupCode.set(code)
-                //       setPhoto(getPhoto(photo))
-
-                //     } else {
-                //       addSubTaskGroupModal.show()
-                //       setSelectedSubTaskGroup(taskGroup)
-                //       const { name, description, code, photo, start_time, end_time } = taskGroup
-                //       subTaskGroupName.set(name)
-                //       subTaskGroupDescription.set(description)
-                //       subTaskGroupCode.set(code)
-                //       setSubTaskPhoto(getPhoto(photo))
-                //       setStatTimeEta(start_time)
-                //       setEndTimeEta(end_time)
-                //       setIsEdit(true)
-                //     }
-                //   }
-                //   else if (el.id === '1') {
-                //     addSubTaskGroupModal.show();
-                //     setIsEdit(false)
-                //     setSelectedSubTaskGroup(taskGroup)
-                //   }
-                //   else if (el.id === '2') {
-                //     const { id } = taskGroup
-                //     changeGroupStatusApiHandler(id, true)
-                //   }
-                //   else if (el.id === '3') {
-                //     const { id } = taskGroup
-                //     changeGroupStatusApiHandler(id, false)
-                //   }
-                //   else if (el.id === '4') {
-                //     const { id } = taskGroup
-
-                //     // addGroupUsers(id)
-                //     addMemberModal.show()
-                //     setGroupId(taskGroup.id)
-
-                //   }
-                // }} 
-                // />
-
-            };
-        });
+                "": <MenuBar menuData={MENU} onClick={(el) => {
+                    if (el.id === 0) {
+                        editVariant(variant)
+                    }
+                }}
+                />
+            }
+        })
     };
 
     return (
@@ -226,8 +181,8 @@ function Group() {
                     {convertedGroupDetails && convertedGroupDetails.length > 0 ?
                         convertedGroupDetails.map((el: any, index: number) => {
                             return (
-                                <div className={'card col justify-content-center m-3'} style={{ height: true ? dynamicHeight.dynamicHeight : '5em' }}>
-                                    <div className="row justify-content-center align-items-center m-2" >
+                                <div className={'card  col mx-2 py-3'} style={{ height: el.show ? dynamicHeight.dynamicHeight : '5em' }}>
+                                    <div className="row justify-content-center  m-2" >
                                         <div className="col">
                                             <h3>{el.name}</h3>
                                         </div>
@@ -241,13 +196,8 @@ function Group() {
                                                 }
                                                 size={"sm"}
                                                 onClick={() => {
-                                                    setShowGroup(!showGroup)
-                                                    // onClickShow(el, index)
-                                                    if (!showGroup) {
-                                                        // getGroupList(taskGroupCurrentPages);
-                                                        // getKnowledgeGroupDetails()
-                                                    }
-
+                                                    onClickShow(el, index)
+                                                    selectedVariant(el)
                                                 }}
                                             />
                                             <Button
@@ -255,26 +205,26 @@ function Group() {
                                                 text={translate("product.addItem")}
                                                 size={"sm"}
                                                 onClick={() => {
-                                                    setSelectedGroup(undefined)
+                                                    onClickAddVariant(el)
                                                     // addTaskGroupModal.show()
                                                 }}
                                             />
                                         </div>
                                     </div>
-                                    {el?.variant ? (
+                                    {el.show && selectedGroupVariant && selectedGroupVariant.length > 0 && (
                                         <CommonTable
                                             isPagination
-                                            displayDataSet={normalizedTaskGroupData(convertedGroupDetails)}
+                                            displayDataSet={normalizedTaskGroupData(selectedGroupVariant)}
                                         />
-                                    ) : (
-                                        <div className="h-100 d-flex justify-content-center align-items-center">
-                                            <NoRecordsFound />
-                                        </div>
-                                    )}
+                                    )
+                                        // : 
+                                        // <div className="h-100 d-flex justify-content-center align-items-center">
+                                        //     <NoRecordsFound />
+                                        // </div>
+                                    }
                                 </div>
                             )
                         })
-
                         : <div className={'d-flex justify-content-center align-items-center'} style={{ height: '90vh' }}><NoDataFound text={"No Data Found"} /></div>
                     }
                 </div>
