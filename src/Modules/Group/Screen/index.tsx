e7afb5a7ba53b9d54e5cafaa6dfa2f46dcd39bb6
purushothamanzenylog <reactjs.1@zenylog.com>
2023-07-22 12:35:46 +0530

    group data

--- src/Modules/Group/Screen/index.tsx
+++ src/Modules/Group/Screen/index.tsx
@@ -35,13 +35,13 @@ function Group() {
             getKnowledgeGroup({
                 params,
                 onSuccess: (success: any) => () => {
-                    const normalizedArray = success?.details?.knowledege_groups && success?.details?.knowledege_groups.length > 0
-                        && success.details.knowledege_groups.map((el: any) => {
-                            let requiredObject = { show: false }
-                            return { ...el, ...requiredObject }
-                        })
+                    // const normalizedArray = success?.details?.knowledege_groups && success?.details?.knowledege_groups.length > 0
+                    //     && success.details.knowledege_groups.map((el: any) => {
+                    //         let requiredObject = { show: false }
+                    //         return { ...el, ...requiredObject }
+                    //     })
 
-                    setConvertedGroupDetails(normalizedArray)
+                    // setConvertedGroupDetails(normalizedArray)
                 },
                 onError: (error: string) => () => {
                 },
@@ -55,17 +55,13 @@ function Group() {
             getKnowledgeGroupVariant({
                 params,
                 onSuccess: (success: any) => () => {
-
+                    filteredGroupDetails(success.details)
                 },
                 onError: (error: string) => () => {
                 },
             })
         );
     };
-
-
-
-
 
 
     const createKnowledgeGroupDetails = () => {
@@ -121,71 +117,97 @@ function Group() {
         variantModal.show()
     }
 
-    // const normalizedTaskGroupData = (data: any) => {
-    //     return data.map((taskGroup: any,) => {
+    // const onClickShow = (item: any, index: number) => {
+    //     console.log("=========>", convertedGroupDetails);
+    //     let showVariant = [...convertedGroupDetails]
+    //     showVariant && showVariant.length > 0 && showVariant.map((el: any) => {
+    //         if (showVariant[index].show) {
+    //             showVariant[index].show = false
+    //         } else {
+    //             showVariant[index].show = true
+    //         }
+    //     })
+    //     setConvertedGroupDetails(showVariant)
+    // }
 
 
+    const filteredGroupDetails = (response) => {
+        let GroupDetailsCopy = [...GroupDetails?.knowledege_groups];
+        response && response?.knowledge_group_varaiant.length > 0 && response.knowledge_group_varaiant.map((element: any, index: number) => {
+            GroupDetailsCopy && GroupDetailsCopy.length > 0 && GroupDetailsCopy.map((el: any) => {
+                if (element?.knowledge_group?.id === el.id) {
+                    GroupDetailsCopy[index].variant = element
+                }
+            })
+        })
+        setConvertedGroupDetails(GroupDetailsCopy)
+    }
 
-    //         return {
-    //             name: <div className="row  align-items-center">
-    //                 <div className="pl-3">
-    //                     <span className={`${marked_as_closed && 'text-primary'}`}>{name}</span>
-    //                     <br></br>
-    //                     {!is_parent && <small> {parent.name}</small>}
-    //                 </div>
-    //             </div >,
-    //             tag: code,
-    //             // "": <MenuBar menuData={getGroupMenuItem(marked_as_closed, is_parent)} onClick={(el) => {
-    //             //   if (el.id === '0') {
-    //             //     if (is_parent) {
-    //             //       addTaskGroupModal.show()
-    //             //       setSelectedTaskGroup(taskGroup)
-    //             //       const { name, description, code, photo } = taskGroup
-    //             //       taskGroupName.set(name)
-    //             //       taskGroupDescription.set(description)
-    //             //       taskGroupCode.set(code)
-    //             //       setPhoto(getPhoto(photo))
+    const normalizedTaskGroupData = (data: any) => {
+        return data.map((taskGroup: any,) => {
 
-    //             //     } else {
-    //             //       addSubTaskGroupModal.show()
-    //             //       setSelectedSubTaskGroup(taskGroup)
-    //             //       const { name, description, code, photo, start_time, end_time } = taskGroup
-    //             //       subTaskGroupName.set(name)
-    //             //       subTaskGroupDescription.set(description)
-    //             //       subTaskGroupCode.set(code)
-    //             //       setSubTaskPhoto(getPhoto(photo))
-    //             //       setStatTimeEta(start_time)
-    //             //       setEndTimeEta(end_time)
-    //             //       setIsEdit(true)
-    //             //     }
-    //             //   }
-    //             //   else if (el.id === '1') {
-    //             //     addSubTaskGroupModal.show();
-    //             //     setIsEdit(false)
-    //             //     setSelectedSubTaskGroup(taskGroup)
-    //             //   }
-    //             //   else if (el.id === '2') {
-    //             //     const { id } = taskGroup
-    //             //     changeGroupStatusApiHandler(id, true)
-    //             //   }
-    //             //   else if (el.id === '3') {
-    //             //     const { id } = taskGroup
-    //             //     changeGroupStatusApiHandler(id, false)
-    //             //   }
-    //             //   else if (el.id === '4') {
-    //             //     const { id } = taskGroup
+            console.log("==========>", taskGroup);
 
-    //             //     // addGroupUsers(id)
-    //             //     addMemberModal.show()
-    //             //     setGroupId(taskGroup.id)
 
-    //             //   }
-    //             // }} 
-    //             // />
+            return {
+                name: <div className="row  align-items-center">
+                    <div className="pl-3">
+                        <span className={`'text-primary'}`}>{taskGroup.variant?.name}</span>
+                        <br></br>
+                    </div>
+                </div >,
+                tag: '',
+                // "": <MenuBar menuData={getGroupMenuItem(marked_as_closed, is_parent)} onClick={(el) => {
+                //   if (el.id === '0') {
+                //     if (is_parent) {
+                //       addTaskGroupModal.show()
+                //       setSelectedTaskGroup(taskGroup)
+                //       const { name, description, code, photo } = taskGroup
+                //       taskGroupName.set(name)
+                //       taskGroupDescription.set(description)
+                //       taskGroupCode.set(code)
+                //       setPhoto(getPhoto(photo))
 
-    //         };
-    //     });
-    // };
+                //     } else {
+                //       addSubTaskGroupModal.show()
+                //       setSelectedSubTaskGroup(taskGroup)
+                //       const { name, description, code, photo, start_time, end_time } = taskGroup
+                //       subTaskGroupName.set(name)
+                //       subTaskGroupDescription.set(description)
+                //       subTaskGroupCode.set(code)
+                //       setSubTaskPhoto(getPhoto(photo))
+                //       setStatTimeEta(start_time)
+                //       setEndTimeEta(end_time)
+                //       setIsEdit(true)
+                //     }
+                //   }
+                //   else if (el.id === '1') {
+                //     addSubTaskGroupModal.show();
+                //     setIsEdit(false)
+                //     setSelectedSubTaskGroup(taskGroup)
+                //   }
+                //   else if (el.id === '2') {
+                //     const { id } = taskGroup
+                //     changeGroupStatusApiHandler(id, true)
+                //   }
+                //   else if (el.id === '3') {
+                //     const { id } = taskGroup
+                //     changeGroupStatusApiHandler(id, false)
+                //   }
+                //   else if (el.id === '4') {
+                //     const { id } = taskGroup
+
+                //     // addGroupUsers(id)
+                //     addMemberModal.show()
+                //     setGroupId(taskGroup.id)
+
+                //   }
+                // }} 
+                // />
+
+            };
+        });
+    };
 
     return (
         <div>
@@ -201,11 +223,11 @@ function Group() {
             </div>
             <div className='mx-3'>
                 <div className='row'>
-                    {convertedGroupDetails?.knowledege_groups && convertedGroupDetails?.knowledege_groups.length > 0 ?
-                        convertedGroupDetails?.knowledege_groups.map((el: any) => {
+                    {convertedGroupDetails && convertedGroupDetails.length > 0 ?
+                        convertedGroupDetails.map((el: any, index: number) => {
                             return (
-                                <div className={'card col justify-content-center m-3'} style={{ height: el?.show ? dynamicHeight.dynamicHeight : '5em' }}>
-                                    <div className="row justify-content-center align-items-center mx-2" >
+                                <div className={'card col justify-content-center m-3'} style={{ height: true ? dynamicHeight.dynamicHeight : '5em' }}>
+                                    <div className="row justify-content-center align-items-center m-2" >
                                         <div className="col">
                                             <h3>{el.name}</h3>
                                         </div>
@@ -220,6 +242,7 @@ function Group() {
                                                 size={"sm"}
                                                 onClick={() => {
                                                     setShowGroup(!showGroup)
+                                                    // onClickShow(el, index)
                                                     if (!showGroup) {
                                                         // getGroupList(taskGroupCurrentPages);
                                                         // getKnowledgeGroupDetails()
@@ -238,6 +261,16 @@ function Group() {
                                             />
                                         </div>
                                     </div>
+                                    {el?.variant ? (
+                                        <CommonTable
+                                            isPagination
+                                            displayDataSet={normalizedTaskGroupData(convertedGroupDetails)}
+                                        />
+                                    ) : (
+                                        <div className="h-100 d-flex justify-content-center align-items-center">
+                                            <NoRecordsFound />
+                                        </div>
+                                    )}
                                 </div>
                             )
                         })
