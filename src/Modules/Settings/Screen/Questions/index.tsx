93b66139368a4819120fba60aec8bdc3748a4f70
Jay <jayakumar@leorainfotech.in>
2023-07-31 20:35:37 +0530

    no message

--- src/Modules/Settings/Screen/Questions/index.tsx
+++ src/Modules/Settings/Screen/Questions/index.tsx
@@ -20,15 +20,14 @@ function Questions() {
     const { selectedRole, questions } = useSelector((state: any) => state.DashboardReducer)
     const name = useInput('');
     const description = useInput('');
-    const [dataGenerated, setDataGenerated] = useState(false);
+    const [dataGenerated, setDateGenerated] = useState(false)
+    console.log('dataGenerated-------->',dataGenerated)
 
 
     useEffect(() => {
         getQuestionsFormApi()
     }, [])
 
-
-    const loader = useLoader(false);
 
 
     const getQuestionsFormApi = () => {
@@ -49,27 +48,28 @@ function Questions() {
 
 
     function proceedGenerateFormApiHandler() {
+        setDateGenerated(true)
+        addGenerateFormModal.hide()
 
         const params = {
             name: name.value,
             description: description.value,
             knowledge_group_variant_id: selectedRole?.id
         }
-        setDataGenerated(true)
 
         dispatch(
             generateForm({
                 params,
                 onSuccess: () => (response) => {
-                    goTo(ROUTES['designation-module']['questions'])
+                    setDateGenerated(false)
                     resetValues()
                     showToast(response.message, 'success')
-                    setDataGenerated(false)
+                    goTo(ROUTES['designation-module']['questions'])
 
                 },
                 onError: () => (error) => {
+                    setDateGenerated(false)
                     showToast(error.error_message)
-                    setDataGenerated(false)
                 },
             })
         );
@@ -83,54 +83,57 @@ function Questions() {
 
     return (
         <>
-            <div className='m-3'>
-                <div className='col text-right ml-3'>
+            {
+                dataGenerated ? null :
+                    <div className='m-3'>
+                        <div className='col text-right ml-3'>
 
-                    <Button
-                        text={'Generate using AI'}
-                        className="text-white"
-                        onClick={addGenerateFormModal.show}
-                    />
+                            <Button
+                                text={'Generate using AI'}
+                                className="text-white"
+                                onClick={addGenerateFormModal.show}
+                            />
 
-                    <Button
-                        text={'Generate by User'}
-                        className="text-white"
-                        onClick={() => {
-                            goTo(ROUTES['designation-module']['create-question-form'])
-                        }}
-                    />
-                </div>
+                            <Button
+                                text={'Generate by User'}
+                                className="text-white"
+                                onClick={() => {
+                                    goTo(ROUTES['designation-module']['create-question-form'])
+                                }}
+                            />
+                        </div>
 
-                <div className='row mt-3'>
-                    {
-                        questions && questions.length > 0 && questions?.map((item: any) => {
-                            const { id, name, description } = item;
-                            return (
-                                <div className='col-4' key={id}>
-                                    <Card className='card justify-content-center '
-                                        onClick={() => {
-                                            goTo(ROUTES['designation-module']['question-sections'])
-                                            dispatch(setSelectedQuestionForm(item))
-                                        }} >
-                                        <h4 className='mb-0 pointer mt--2'>{name}</h4>
-                                        <div className={'mx--4'}><Divider space={'3'} /></div>
-                                        <small className='mb-0 pointer'>{description}</small>
-                                    </Card>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                        <div className='row mt-3'>
+                            {
+                                questions && questions.length > 0 && questions?.map((item: any) => {
+                                    const { id, name, description } = item;
+                                    return (
+                                        <div className='col-4' key={id}>
+                                            <Card className='card justify-content-center '
+                                                onClick={() => {
+                                                    goTo(ROUTES['designation-module']['question-sections'])
+                                                    dispatch(setSelectedQuestionForm(item))
+                                                }} >
+                                                <h4 className='mb-0 pointer mt--2'>{name}</h4>
+                                                <div className={'mx--4'}><Divider space={'3'} /></div>
+                                                <small className='mb-0 pointer'>{description}</small>
+                                            </Card>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
 
-            </div >
-
-            {dataGenerated ? <AnalyzingAnimation /> :
-                <Modal title={'Generate Form'} isOpen={addGenerateFormModal.visible} onClose={addGenerateFormModal.hide}>
-                    <Input className={'col-6'} heading={'Name'} value={name.value} onChange={name.onChange} />
-                    <Input className={'col-6'} heading={'Description'} value={description.value} onChange={description.onChange} />
-                    <Button text={'Submit'} onClick={proceedGenerateFormApiHandler} />
-                </Modal>
+                    </div >
             }
+
+
+            <Modal title={'Generate Form'} isOpen={addGenerateFormModal.visible} onClose={addGenerateFormModal.hide}>
+                <Input className={'col-6'} heading={'Name'} value={name.value} onChange={name.onChange} />
+                <Input className={'col-6'} heading={'Description'} value={description.value} onChange={description.onChange} />
+                <Button text={'Submit'} onClick={proceedGenerateFormApiHandler} />
+            </Modal>
+            {dataGenerated && <AnalyzingAnimation />}
         </>
     )
 }
