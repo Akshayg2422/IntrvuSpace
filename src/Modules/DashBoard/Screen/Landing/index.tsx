50048168986e4c2e2573fd4c198ce32acf57c78f
TAMILSELVAN\tamil_hfh9g6g <tamilselvan.intern@leorainfotech.in>
2023-08-08 17:28:24 +0530

    no message

--- src/Modules/DashBoard/Screen/Landing/index.tsx
+++ src/Modules/DashBoard/Screen/Landing/index.tsx
@@ -6,21 +6,25 @@ import { useNavigation } from '@Hooks'
 function Landing() {
     const { goTo } = useNavigation()
     return (
-        <div className="container-fluid bg-primary h-100vh p-3">
-            <div className='col text-right'>
+        <div className="container-fluid bg-primary h-100vh">
+            <div className='col text-right pt-3'>
                 <Button color={'white'} text={'Login'} size={'md'} onClick={() => {
                     goTo(ROUTES['auth-module'].login);
                 }} />
             </div>
-            <div className="row justify-content-center align-items-center">
-                <div className="row align-items-center justify-content-center col-12">
+            <div className="row justify-content-center align-items-center "
+            style={{
+                height:'80vh'
+            }}
+            >
+                <div className="row  justify-content-center">
                     <h1 className='pb-0 mb--1 font-weight-bolder text-white'
                         style={{
                             fontSize: '13vh'
                         }}
                     >MOCK <b className='text-black'>EAZY</b></h1>
                 </div>
-                <div className="row align-items-center justify-content-center col-12">
+                <div className="row justify-content-center col-12 ">
                     <p className="text-center text-white "
                         style={{
                             maxWidth: '85vh'
@@ -28,13 +32,17 @@ function Landing() {
                     >
                         Interview App is the easiest way to interview people automatically. Invite people to answer your question spontaneously with their webcam. Where
                     </p>
+
                 </div>
-                <Image
-                    className=""
-                    src={image.LOGIN_SIDE_IMAGE}
-                    height={'35%'}
-                    width={'35%'}
-                />
+                <div className="row justify-content-center col-12 ">
+                    <Image
+                        className=""
+                        src={image.LOGIN_SIDE_IMAGE}
+                        height={'30%'}
+                        width={'30%'}
+                    />
+                </div>
+
             </div>
         </div>
     )
