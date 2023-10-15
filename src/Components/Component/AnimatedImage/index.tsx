9f657744ef16cfac852439975edbdd952ee75bc2
Jayakumar <jayakumar@leorainfotech.in>
2023-10-15 13:51:47 +0530

    web cam fix

--- src/Components/Component/AnimatedImage/index.tsx
+++ src/Components/Component/AnimatedImage/index.tsx
@@ -1,6 +1,7 @@
-import { AnimatedLoader } from "@Components";
+import { AnimatedLoader, WebCamRecorder } from "@Components";
 import classNames from "classnames";
 import "./AnimatedImageFrame.scss";
+
 
 const AnimatedImage = ({ name, shouldBlink, show, showWebCam = false, isMuted = false, variant = 'lg' }) => {
   const imageClasses = classNames(!isMuted ? variant === 'sm' ? "animated-image-small" : "animated-image" : "", { blink: shouldBlink });
@@ -14,11 +15,16 @@ const AnimatedImage = ({ name, shouldBlink, show, showWebCam = false, isMuted = 
         }}
       >
         {!show ?
-          <div className="d-flex align-items-center justify-content-center">
-            <h1 className="text-white font-weight-700" style={{
-              fontSize: variant === "sm" ? "40px" : "70px"
-            }}>{name}
-            </h1>
+          <div>
+            {showWebCam ?
+              <WebCamRecorder /> :
+              <div className="d-flex align-items-center justify-content-center">
+                <h1 className="text-white font-weight-700" style={{
+                  fontSize: variant === "sm" ? "40px" : "70px"
+                }}>{name}
+                </h1>
+              </div>
+            }
           </div> :
           <div className="col text-center">
             <AnimatedLoader variant={variant} />
