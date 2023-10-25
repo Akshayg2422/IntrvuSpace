19e9fb7573d0584e02dbd1642811ef2fb6503336
Jay <jayakumar@leorainfotech.in>
2023-10-25 11:50:40 +0530

    no message

--- src/Modules/Settings/Container/PreparingYourInterview/index.tsx
+++ src/Modules/Settings/Container/PreparingYourInterview/index.tsx
@@ -7,7 +7,6 @@ import { useSelector } from 'react-redux';
 function PreparingYourInterview() {
     const { goBack } = useNavigation();
     const { loginDetails } = useSelector((state: any) => state.AppReducer);
-    const generateJdModal = useModal(true);
 
     const steps = [
         { icon: icons.check, text: 'Analyzing your JD' },
@@ -30,35 +29,33 @@ function PreparingYourInterview() {
     }, []);
 
     return (
-        // <Modal isOpen={generateJdModal.visible} onClose={generateJdModal.hide}>
 
-            <div className="col-md-12 rounded-sm px-md-4">
-                <div className="text-secondary col-11">
-                    <div className="m-0 h2 font-weight-800 pb-2">Preparing your Interview...</div>
-                    <div className="text-default">It will take a couple of minutes. You can wait and join using the link that will be sent to your email once the interview is ready.</div>
-                    <div className="pt-5 text-secondary pb-md-6 pb-sm-0 pb-5">
-                        {steps.map((step, index) => (
-                            <div key={index} className="d-flex align-items-center pb-2">
-                                {index <= currentStep ? (
-                                    <>
-                                        <img src={step.icon} height={22} />
-                                        <small className="pl-2 m-0 font-weight-500 text-black">{step.text}</small>
-                                    </>
-                                ) : (
-                                    <small style={{paddingLeft:'19px'}} className="m-0 text-default">{step.text}</small>
-                                )}
-                            </div>
-                        ))}
-                    </div>
+        <div className="col-md-12 rounded-sm px-md-4">
+            <div className="text-secondary col-11">
+                <div className="m-0 h2 font-weight-800 pb-2">Preparing your Interview...</div>
+                <div className="text-default">It will take a couple of minutes. You can wait and join using the link that will be sent to your email once the interview is ready.</div>
+                <div className="pt-5 text-secondary pb-md-6 pb-sm-0 pb-5">
+                    {steps.map((step, index) => (
+                        <div key={index} className="d-flex align-items-center pb-2">
+                            {index <= currentStep ? (
+                                <>
+                                    <img src={step.icon} height={22} />
+                                    <small className="pl-2 m-0 font-weight-500 text-black">{step.text}</small>
+                                </>
+                            ) : (
+                                <small style={{ paddingLeft: '19px' }} className="m-0 text-default">{step.text}</small>
+                            )}
+                        </div>
+                    ))}
                 </div>
-                {loginDetails?.isLoggedIn && (
-                    <div className="d-flex justify-content-center pr-sm-0 pr-5">
-                        <Button className={'px-5 border border-primary rounded-sm'} text={'Start Interview'} onClick={() => { }} />
-                    </div>
-                )}
             </div>
+            {loginDetails?.isLoggedIn && (
+                <div className="d-flex justify-content-center pr-sm-0 pr-5">
+                    <Button className={'px-5 border border-primary rounded-sm'} text={'Start Interview'} onClick={() => { }} />
+                </div>
+            )}
+        </div>
 
-        // </Modal>
     );
 }
 
