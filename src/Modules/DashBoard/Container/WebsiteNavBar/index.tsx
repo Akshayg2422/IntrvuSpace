8a93caba5a3d17f84c555f7dce3947642b5c62d0
Jay <jayakumar@leorainfotech.in>
2023-10-30 12:56:41 +0530

    no message

--- src/Modules/DashBoard/Container/WebsiteNavBar/index.tsx
+++ src/Modules/DashBoard/Container/WebsiteNavBar/index.tsx
@@ -4,24 +4,16 @@ import {
   Container,
   NavbarBrand,
   UncontrolledCollapse,
-  Row,
-  Col,
   Nav,
-  NavItem,
-  NavLink,
 } from "reactstrap";
 import "./index.css";
 import { icons } from "@Assets";
 import { Image, Button } from "@Components";
 import { ROUTES } from "@Routes";
-import { useDynamicHeight, useNavigation } from "@Hooks";
+import { useNavigation } from "@Hooks";
 
 function WebsiteNavBar() {
   const { goTo } = useNavigation();
-
-  let dynamicHeight: any = useDynamicHeight();
-
-  const dynamicPaddingTop = dynamicHeight.dynamicWidth <= 576 ? "pt-2" : "";
 
   return (
     <Navbar
@@ -32,8 +24,8 @@ function WebsiteNavBar() {
     >
       <Container>
         <NavbarBrand to="/" tag={Link}>
-          <div className={"ml-sm--5 ml--4"} >
-            <img alt="..." src={icons.logoText} style={{ height: 55, zIndex: 100 }} />
+          <div className={""} >
+            {/* <img src={icons.logoText} alt="Logo" style={{ height: '20%', width: '20%' }} /> */}
           </div>
         </NavbarBrand>
         <button
@@ -46,7 +38,7 @@ function WebsiteNavBar() {
           id="navbar-collapse"
           type="button"
         >
-          <div className={"pt-2"}>
+          <div className={"pt-md-2"}>
             <Image height={30} width={30} src={icons.navbarToggler} />
           </div>
         </button>
@@ -73,8 +65,8 @@ function WebsiteNavBar() {
               style={{
                 borderColor: "#d8dade",
                 // borderWidth: 1,
-                paddingTop:10,
-                paddingBottom:10,
+                paddingTop: 10,
+                paddingBottom: 10,
                 fontSize: 14,
                 paddingRight: 15,
                 paddingLeft: 15,
