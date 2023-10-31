07ac4d698ebc4ed31aa4f0b4b40aa68b1004ec9b
Jay <jayakumar@leorainfotech.in>
2023-10-31 19:03:43 +0530

    no message

--- src/Services/Api/ApiHelper/index.js
+++ src/Services/Api/ApiHelper/index.js
@@ -9,10 +9,10 @@ export const BUILD_TYPE_PRE_PROD_IP = 5;
 
 const LIVE_IP = "https://mepp.leorainfotech.in/"
 const PRE_PROD_IP = "https://mepp.leorainfotech.in/"
-const LOCAL_IP = "http://192.168.151.126:8005"
+const LOCAL_IP = "http://192.168.10.5:8004"
 
 
-export const BUILD_TYPE = BUILD_TYPE_PRE_PROD_IP;
+export const BUILD_TYPE = BUILD_TYPE_LOCAL;
 export const SERVER =
   BUILD_TYPE === BUILD_TYPE_LIVE
     ? LIVE_IP
@@ -28,7 +28,7 @@ export const SERVER =
 
 
 
-export const CALL_WEBSOCKET = `wss://mepp.leorainfotech.in/aaa`
+export const CALL_WEBSOCKET = `ws://192.168.10.5:8004/aaa`
 
 
 const axiosApi = axios.create({
