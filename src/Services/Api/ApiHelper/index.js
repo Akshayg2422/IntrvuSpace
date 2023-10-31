973bb88a0498fd041e2ee6abd74b5ee6287f9a5c
Jay <jayakumar@leorainfotech.in>
2023-10-31 17:09:39 +0530

    no message

--- src/Services/Api/ApiHelper/index.js
+++ src/Services/Api/ApiHelper/index.js
@@ -9,10 +9,10 @@ export const BUILD_TYPE_PRE_PROD_IP = 5;
 
 const LIVE_IP = "https://mepp.leorainfotech.in/"
 const PRE_PROD_IP = "https://mepp.leorainfotech.in/"
-const LOCAL_IP = "http://192.168.10.5:8004"
+const LOCAL_IP = "http://192.168.151.126:8005"
 
 
-export const BUILD_TYPE = BUILD_TYPE_LOCAL;
+export const BUILD_TYPE = BUILD_TYPE_PRE_PROD_IP;
 export const SERVER =
   BUILD_TYPE === BUILD_TYPE_LIVE
     ? LIVE_IP
