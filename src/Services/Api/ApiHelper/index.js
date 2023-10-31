d9c2bef0cb0049b58a6519c560b340fcaad439b3
DIVA\prabh <dhivyaprabhandan@leorainfotech.in>
2023-10-31 21:40:10 +0530

    report screen changes

--- src/Services/Api/ApiHelper/index.js
+++ src/Services/Api/ApiHelper/index.js
@@ -7,7 +7,7 @@ export const BUILD_TYPE_LOCAL = 4;
 export const BUILD_TYPE_PRE_PROD_IP = 5;
 
 
-const LIVE_IP = "https://mepp.leorainfotech.in/"
+const LIVE_IP = "https://mockeazyprimary.leorainfotech.in/"
 const PRE_PROD_IP = "https://mepp.leorainfotech.in/"
 const LOCAL_IP = "http://192.168.151.126:8005"
 
