bced38359f5ea126722b1734b524ca7d171d4725
Jayakumar <jayakumar@leorainfotech.in>
2023-11-15 20:38:01 +0530

    from jd

--- src/Modules/Settings/Container/PreparingYourInterview/index.tsx
+++ src/Modules/Settings/Container/PreparingYourInterview/index.tsx
@@ -1,63 +1,103 @@
-import React, { useState, useEffect } from 'react';
 import { icons } from '@Assets';
-import { Button, Image } from '@Components';
-import { PreparingYourInterviewProps } from './interfaces'
+import { Image } from '@Components';
+import { useEffect, useState } from 'react';
+import { PreparingYourInterviewProps } from './interfaces';
+import './index.css'
 
-function PreparingYourInterview({ showStart, onClick }: PreparingYourInterviewProps) {
+function PreparingYourInterview({ isCompleted }: PreparingYourInterviewProps) {
 
     const steps = [
         { icon: icons.check, text: 'Analyzing your JD' },
         { icon: icons.check, text: 'Extracting key areas' },
         { icon: icons.check, text: 'Preparing Topics' },
         { icon: icons.check, text: 'Generating evaluation criteria' },
-        { icon: showStart ? icons.check : null, text: 'Preparing interview Room' },
+        { icon: icons.check, text: 'Preparing interview Room' },
     ];
 
     const [currentStep, setCurrentStep] = useState(-1);
 
+
     useEffect(() => {
-        const timer = setInterval(() => {
-            setCurrentStep((prevStep) => (prevStep < steps.length - 1 ? prevStep + 1 : prevStep));
-        }, 5000);
+
+        if (isCompleted) {
+            setCurrentStep(steps.length - 1)
+        }
+    }, [isCompleted])
+
+
+
+    useEffect(() => {
+
+        let timer: NodeJS.Timeout;
 
         const initialStepTimer = setTimeout(() => {
             setCurrentStep(0);
-        }, 1000);
+            timer = setInterval(() => {
+                setCurrentStep((prevStep) => (prevStep < steps.length - 2 ? prevStep + 1 : prevStep));
+            }, 5000);
+        }, 2000);
 
         return () => {
-            clearInterval(timer);
+            if (timer) {
+                clearInterval(timer);
+            }
             clearTimeout(initialStepTimer);
         };
     }, []);
 
+
+
     return (
         <div>
-            <div className="text-secondary">
-                <div className="text-secondary">
-                    {steps.map((step, index) => (
-                        <div key={index} className="d-flex align-items-center pb-2">
-                            {index <= currentStep ? (
-                                <>
-                                    {index === 4 ? !step.icon ? <span style={{ marginLeft: '2%' }}></span> : <Image src={step.icon} height={12} width={12} style={{
-                                        objectFit: 'contain'
-                                    }} /> : <Image src={step.icon} height={12} width={12} style={{
-                                        objectFit: 'contain'
-                                    }} />}
-                                    <small className="pl-2 m-0 font-weight-500 text-black">{step.text}</small>
-                                </>
-                            ) : (
-                                <small style={{ paddingLeft: '19px' }} className="m-0 text-default">{step.text}</small>
-                            )}
+            {
+                steps.map((each, index) => {
+
+                    const isSelected = index <= currentStep
+
+                    const { text, icon } = each
+
+                    return (
+                        <div key={index} className={'d-flex align-items-center mb-2'}>
+
+                            <div className={'d-flex align-items-center'}>
+                                {
+                                    isSelected ?
+
+                                        <Image
+                                            className='mr-2'
+                                            src={icon}
+                                            height={12}
+                                            width={12}
+                                            style={{
+                                                objectFit: 'contain'
+                                            }}
+                                        />
+
+                                        :
+                                        <>
+                                            {
+                                                currentStep !== -1
+                                                    ?
+                                                    <div
+                                                        className='mr-2'
+                                                        style={{
+                                                            width: 12,
+                                                            height: 12
+                                                        }}>
+
+                                                    </div>
+                                                    :
+                                                    null
+                                            }
+                                        </>
+                                }
+                            </div>
+                            <div className={`key-point  ${isSelected ? 'text-secondary  mb-0' : ''}`}>{text}</div>
                         </div>
-                    ))}
-                </div>
-            </div>
-            {showStart && (
-                <div className="d-flex justify-content-center">
-                    <Button className={'border border-primary rounded-sm'} text={'Start Interview'} onClick={onClick} />
-                </div>
-            )}
-        </div>
+                    )
+                })
+            }
+        </div >
     );
 }
 
