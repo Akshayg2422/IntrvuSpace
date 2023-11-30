9138342d01091a3d9377034cb6504daae1e6e489
Jayakumar <jayakumar@leorainfotech.in>
2023-11-30 23:49:56 +0530

    screen heading fixes

--- src/Components/Component/ScreenHeading/index.tsx
+++ src/Components/Component/ScreenHeading/index.tsx
@@ -3,10 +3,13 @@ import { ScreenHeadingProps } from './interfaces'
 import { useNavigation } from '@Hooks'
 import { Image } from '@Components'
 import { icons } from '@Assets'
+import './index.css'
 
 function ScreenHeading({ text, subtitle, children }: ScreenHeadingProps) {
 
     const { goBack } = useNavigation()
+
+
 
     return (
 
@@ -14,27 +17,21 @@ function ScreenHeading({ text, subtitle, children }: ScreenHeadingProps) {
             <div className={'d-flex'}>
                 <div className="screen-heading col-auto m-0 p-0 d-flex align-items-center" style={{ position: 'relative' }}>
                     <div
-                        style={{
-                            position: 'absolute',
-                            top: '50%',
-                            left: '-40px',
-                            transform: 'translate(0, -57%)'
-                        }}>
+                        className='pointer back-btn-container'
+                        onClick={() => { goBack() }}
+                    >
                         <Image
+                            className={'btn-image-container'}
                             src={icons.backButton}
-                            height={15}
-                            width={15}
-                            style={{
-                                objectFit: 'contain',
-                            }}
                         />
                     </div>
-                    <div>
+                    <div className='m-0'>
                         {text}
                     </div>
                 </div>
                 <div className={'col'}>{children}</div>
             </div>
+
             <div className={'text-secondary'}>
                 {subtitle}
             </div>
