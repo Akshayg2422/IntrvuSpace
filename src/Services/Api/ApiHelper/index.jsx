ea873eaea4e650a825941b9f01ec63aa9ae8e019
DIVA\prabh <dhivyaprabhandan@leorainfotech.in>
2023-12-14 12:06:11 +0530

    api url changes

--- src/Services/Api/ApiHelper/index.jsx
+++ src/Services/Api/ApiHelper/index.jsx
@@ -17,7 +17,7 @@ const PRE_PROD_IP = "https://mepp.leorainfotech.in"
 const LOCAL_IP = "http://192.168.111.126:8006"
 
 
-export const BUILD_TYPE = BUILD_TYPE_LOCAL;
+export const BUILD_TYPE = BUILD_TYPE_PRE_PROD_IP;
 
 
 export const SERVER =
@@ -36,9 +36,9 @@ export const SERVER =
 
 
 // export const CALL_WEBSOCKET = `wss://mockeazyprimary.leorainfotech.in/aaa`
-// export const CALL_WEBSOCKET = `wss://mepp.leorainfotech.in/aaa`
+export const CALL_WEBSOCKET = `wss://mepp.leorainfotech.in/aaa`
 
-export const CALL_WEBSOCKET = `ws://192.168.111.126:8006/aaa`
+// export const CALL_WEBSOCKET = `ws://192.168.111.126:8006/aaa`
 
 
 const axiosApi = axios.create({
